29b9d754088790866bfd0915650548e7638144df
harsha-headrun <harsha@notemonk.com>
2016-11-28 16:48:55 +0530

    Added colors to graphs and disabled bubbles for zero users

--- assets/js/supervisorapp.js
+++ assets/js/supervisorapp.js
@@ -13,8 +13,10 @@
     for(var i=0;i<data.length;i++){
       if(data[i]['objective_type']=='RANGE'){
         console.log('RANGE');
-        var card = document.querySelector('.card').cloneNode(true);
+        var card = document.querySelector('.cardcopy').cloneNode(true);
         card.classList.add('objective'+data[i]['obj_id']);
+        card.classList.remove('cardcopy');
+        
         document.querySelector('.cards').appendChild(card);
         $('.objective'+data[i]['obj_id']).attr('title',data[i]['objective_name']);
         $('.objective'+data[i]['obj_id']).attr('obj_id',data[i]['obj_id']);
@@ -23,6 +25,7 @@
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').first().children('.col-lg-4').children('.pointsbox').children('.points').html(data[i]['obj_points']);
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar4').remove();
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').addClass('pend');
+
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.percentagefields').children('.pull-left').html(parseInt(data[i]['seg_bad_start_percentage'])+'%');
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.percentagefields').children('.pull-right').html(parseInt(data[i]['seg_bad_end_percentage'])+'%');
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.percentagefields').children('.pull-right').html(parseInt(data[i]['seg_good_end_percentage'])+'%');
@@ -31,25 +34,86 @@
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').css('width',parseInt(data[i]['seg_bad_end_percentage'])+'%');
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').css('width',(parseInt(data[i]['seg_good_end_percentage'])-parseInt(data[i]['seg_bad_end_percentage']))+'%');
         $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').css('width',(100-parseInt(data[i]['seg_good_end_percentage']))+'%');  
+        
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').children('.tooltip-inner').html("<i class='fa fa-user' aria-hidden='true'></i> "+data[i]['seg_bad_percentage']);
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').children('.tooltip-inner').html("<i class='fa fa-user' aria-hidden='true'></i> "+data[i]['seg_good_percentage']);
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').children('.tooltip-inner').html("<i class='fa fa-user' aria-hidden='true'></i> "+data[i]['seg_vgood_percentage']);
+        
+        // for colors red,orange,green
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').css('background-color','#d9534f');  
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').css('background-color','orange');  
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').css('background-color','lightgreen');  
+        //arrow and tooltipinner
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').children('.tooltip-arrow').css('border-top-color','#d9534f');  
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').children('.tooltip-inner').css('background-color','#d9534f');  
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').children('.tooltip-arrow').css('border-top-color','orange');  
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').children('.tooltip-inner').css('background-color','orange');  
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').children('.tooltip-arrow').css('border-top-color','lightgreen');  
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').children('.tooltip-inner').css('background-color','lightgreen');  
+                                
+
+
+        if(data[i]['seg_bad_percentage']===0){
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').hide();  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.percentagefields').addClass('removedpercentagefields');  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.percentagefields').removeClass('percentagefields');
+        }
+        if(data[i]['seg_good_percentage']===0){
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').hide();  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.percentagefields').addClass('removedpercentagefields');  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.percentagefields').removeClass('percentagefields');
+        }
+        if(data[i]['seg_vgood_percentage']===0){
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').hide();  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.percentagefields').addClass('removedpercentagefields');  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.percentagefields').removeClass('percentagefields');
+        }
+
       }else{
 
-      var card = document.querySelector('.card').cloneNode(true);
-      card.classList.add('objective'+data[i]['obj_id']);
-      document.querySelector('.cards').appendChild(card);
-      $('.objective'+data[i]['obj_id']).attr('title',data[i]['objective_name']);
-      $('.objective'+data[i]['obj_id']).attr('obj_id',data[i]['obj_id']);
+        var card = document.querySelector('.cardcopy').cloneNode(true);
+        card.classList.add('objective'+data[i]['obj_id']);
+        card.classList.remove('cardcopy');
+        document.querySelector('.cards').appendChild(card);
+        $('.objective'+data[i]['obj_id']).attr('title',data[i]['objective_name']);
+        $('.objective'+data[i]['obj_id']).attr('obj_id',data[i]['obj_id']);
       
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').first().children('.col-lg-8').children('.card-title').html(data[i]['objective_name']);
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').first().children('.col-lg-4').children('.pointsbox').children('.points').html(data[i]['obj_points']);
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').first().children('.col-lg-8').children('.card-title').html(data[i]['objective_name']);
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').first().children('.col-lg-4').children('.pointsbox').children('.points').html(data[i]['obj_points']);
+
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').children('.tooltip-inner').html(data[i]['percent']['0to25percentage']);
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').children('.tooltip-inner').prepend("<i class='fa fa-user' aria-hidden='true'></i> "); 
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').children('.tooltip-inner').html(data[i]['percent']['26to50percentage']);
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').children('.tooltip-inner').prepend("<i class='fa fa-user' aria-hidden='true'></i> "); 
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').children('.tooltip-inner').html(data[i]['percent']['51to75percentage']);
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').children('.tooltip-inner').prepend("<i class='fa fa-user' aria-hidden='true'></i> "); 
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar4').children('.tooltip').children('.tooltip-inner').html(data[i]['percent']['76to100percentage']);
+        $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar4').children('.tooltip').children('.tooltip-inner').prepend("<i class='fa fa-user' aria-hidden='true'></i> "); 
       
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').children('.tooltip-inner').html(data[i]['percent']['0to25percentage']);
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').children('.tooltip-inner').prepend("<i class='fa fa-user' aria-hidden='true'></i> "); 
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').children('.tooltip-inner').html(data[i]['percent']['26to50percentage']);
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').children('.tooltip-inner').prepend("<i class='fa fa-user' aria-hidden='true'></i> "); 
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').children('.tooltip-inner').html(data[i]['percent']['51to75percentage']);
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').children('.tooltip-inner').prepend("<i class='fa fa-user' aria-hidden='true'></i> "); 
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar4').children('.tooltip').children('.tooltip-inner').html(data[i]['percent']['76to100percentage']);
-      $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar4').children('.tooltip').children('.tooltip-inner').prepend("<i class='fa fa-user' aria-hidden='true'></i> "); 
+        if(data[i]['percent']['0to25percentage']===0){
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.tooltip').hide();  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.percentagefields').addClass('removedpercentagefields');  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar1').children('.percentagefields').removeClass('percentagefields');
+        }
+        if(data[i]['percent']['26to50percentage']===0){
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.tooltip').hide();  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.percentagefields').addClass('removedpercentagefields');  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar2').children('.percentagefields').removeClass('percentagefields');
+      
+        }
+        if(data[i]['percent']['51to75percentage']===0){
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.tooltip').hide();  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.percentagefields').addClass('removedpercentagefields');  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar3').children('.percentagefields').removeClass('percentagefields');
+      
+        }
+        if(data[i]['percent']['76to100percentage']===0){
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar4').children('.tooltip').hide();  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar4').children('.percentagefields').addClass('removedpercentagefields');  
+          $('.objective'+data[i]['obj_id']).children('.card-block').children('.row').last().children('.col-lg-12').children('.progress').children('.pbar4').children('.percentagefields').removeClass('percentagefields');
+      
+        }
+
       }     
     }
     $('.loader').delay(500).fadeOut('slow');
